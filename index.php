197363e13eea87e5bda41d5c554b7f95f4f75814
Ra-Yassine <rayssiyassine@gmail.com>
2026-01-22 20:12:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,5 @@
  git add index.php ( ou bien [git add.] pour ajouter tous les fichiers qui existent dans le dossier)
  commit ? c'est une confirmation dans un depot git
  git commit -m "message"
+git push --set-upstream origin master  // definir le dossier master comme dossier pour enregistrer les fichiers
 -->
