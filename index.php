82ded8bb572bf2324be8240060e1043b4a2242a6
Ra-yassine <rayssiyassinegmail.com>
2026-01-22 20:05:14 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<!--
+
+ git init 
+ git remote add origin
+ git status
+ git add index.php ( ou bien [git add.] pour ajouter tous les fichiers qui existent dans le dossier)
+ commit ? c'est une confirmation dans un depot git
+ git commit -m "message"
+-->
